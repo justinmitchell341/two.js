3b870c6f256728a97512ea134c381d3330308636
justinmitchell341 <jmitchell11@ewu.edu>
2022-05-31 21:13:26 -0700

    Setting up base of two.js and editing over boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# My-Boilerplate
-code boilerplate
+# Two js library
+Two.js is a javascript animation library that makes allows for easy js animation and shape creation
--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("This is a test");
+// https://cdnjs.cloudflare.com/ajax/libs/two.js/0.8.7/two.min.js
+
+// Get drawing for drawing
+const container = document.querySelector("section");
+
+// Set parameters for container
+let params = {
+  width: 500,
+  height: 500
+};
+
+// `new` instantiation Two.js
+const two = new Two(params);
+// append to `container`
+two.appendTo(container);
+
+// Draw Shape
+let shape = two.makeRectangle(250, 250, 100, 100);
+
+// PROPERTY: set fill to red
+shape.fill = "red";
+// METHOD: run method to remove stroke
+shape.noStroke();
+
+// rotate shape. Default is radians
+shape.rotation = Math.PI * 0.25;
+
+// Bind to "Update" function
+two.bind("update", () => {
+  shape.rotation += 0.05;
+});
+
+// Draw to page
+two.play();
